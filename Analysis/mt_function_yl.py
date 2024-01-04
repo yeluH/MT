586abea8dd12fb383d34f1720ead9c567772ac42
yeluH <92099219+yeluH@users.noreply.github.com>
2024-01-04 13:51:10 +0100

    Functions 1st version

---
+++ Analysis/mt_function_yl.py
@@ -0,0 +1,297 @@
+def show_anns(anns):
+    if len(anns) == 0:
+        return
+    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
+    ax = plt.gca()
+    ax.set_autoscale_on(False)
+
+    img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 4))
+    img[:,:,3] = 0
+    for ann in sorted_anns:
+        m = ann['segmentation']
+        color_mask = np.concatenate([np.random.random(3), [0.35]])
+        img[m] = color_mask
+    ax.imshow(img)
+
+def show_mask(mask, ax, random_color=False):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        color = np.array([30/255, 144/255, 255/255, 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+    
+def show_points(coords, labels, ax, marker_size=375):
+    pos_points = coords[labels==1]
+    neg_points = coords[labels==0]
+    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
+    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)   
+    
+def show_box(box, ax):
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))
+
+# Add function def show_anns (borrowed from one of the Jupyter notebooks).
+def show_anns(anns):
+    if len(anns) == 0:
+        return
+    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
+    ax = plt.gca()
+    ax.set_autoscale_on(False)
+    # polygons = []
+    # color = []
+    img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 4))
+    img[:,:,3] = 0
+    for ann in sorted_anns:
+        m = ann['segmentation']
+        color_mask = np.concatenate([np.random.random(3), [0.35]])
+        img[m] = color_mask
+    ax.imshow(img)
+    # for ann in sorted_anns:
+    #     m = ann['segmentation']
+    #     img = np.ones((m.shape[0], m.shape[1], 3))
+    #     color_mask = np.random.random((1, 3)).tolist()[0]
+    #     for i in range(3):
+    #         img[:,:,i] = color_mask[i]
+    #     ax.imshow(np.dstack((img, m*0.35)))
+
+def convert_mask(masks):
+    mb01 = []
+    mb02 = []
+    mb = []
+    n = len(masks)
+    for i in range(0, n):
+        mb01.append(masks[i]['segmentation'])
+    for i in range(0, n):
+        mb02.append(mb01[i]*1)
+    for i in range(0, n):
+        mb.append(mb02[i].astype('uint8'))
+    return mb
+
+def output_mask(im):
+    im1 = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+    mask_r = mask_generator.generate(im1)
+    mask_c = convert_mask(mask_r)
+    return mask_c
+
+# Function to find and plot filtered contours (find-filter-plot-contour)
+def ffpcontour(image, mask, i):
+    image_masked = cv2.bitwise_and(image,image,mask = mask[i])
+    assert image is not None, "image file could not be read, check with os.path.exists()"
+    assert mask is not None, "mask file could not be read, check with os.path.exists()"
+    # imgray = cv2.cvtColor(image_masked, cv2.COLOR_BGR2GRAY)
+    # ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+    ret, thresh = cv2.threshold((mask[i]*255), 127, 255, 0)
+    
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    if len(contours) == 1:
+        contour_f = contours
+    else:
+        contour_f = []
+        for i in range(0, len(contours)):
+            # print(i, "len", len(contours[i]))
+            if len(contours[i]) > 80:
+                contour_f.append(contours[i])
+    # print("filtered", "len", len(contour_f), contour_f)
+    # Plotting the filtered contour
+    # -1 is the contourIdx, (0,255,0) is color, 3 is the thickness
+    # print("raw",len(contours))
+    # print("filtered", len(contour_f))
+    img_con = cv2.drawContours(image_masked, contour_f, -1, (0,255,0), 3) 
+    plt.figure(figsize = (15,15))
+    plt.imshow(img_con)
+    plt.axis('on')
+    plt.show
+    return contour_f
+
+# Function to find and plot filtered contours (find-filter-plot-contour)
+def ffpcontour_noplot(image, mask, i):
+    assert image is not None, "image file could not be read, check with os.path.exists()"
+    assert mask is not None, "mask file could not be read, check with os.path.exists()"
+    # imgray = cv2.cvtColor(image_masked, cv2.COLOR_BGR2GRAY)
+    # ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+    ret, thresh = cv2.threshold((mask[i]*255), 127, 255, 0)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    ll = [] # length list
+    for i in range(0, len(contours)):
+          ll.append(len(contours[i]))
+    maxl = max(ll)
+    maxindex = ll.index(maxl)
+    if (len(contours) == 1):
+        contour_f = contours
+    # elif (maxl < 80):
+    #      contour_f = contours[maxindex]
+    else:
+        contour_f = []
+        for i in range(0, len(contours)):
+            # print(i, "len", len(contours[i]))
+            if (len(contours[i]) >= 80):
+                contour_f.append(contours[i])
+            else:
+                contour_f = contour_f
+        # print("filtered", "len", len(contour_f), contour_f) 
+    # print("raw",len(contours))
+    # print("filtered", len(contour_f))
+    return contour_f
+
+def cgr(contour):
+    assert contour is not None, "image file could not be read, check with os.path.exists()"
+    c = contour
+    isconvex = cv2.isContourConvex(c) # Checking convexity
+    (x,y), (w,h), ar = cv2.minAreaRect(c) # Rotated rectangle with minimum area
+    M = cv2.moments(c) # Moments
+    area = cv2.contourArea(c) # Area 
+    if (M['m00'] != 0):
+        cx = int(M['m10']/M['m00']) # Centroid
+        cy = int(M['m01']/M['m00'])
+    else:
+        cx = x
+        cy = y
+    xs,ys,ws,hs = cv2.boundingRect(c) # Straight bounding rectangle
+    aspect_ratio_wh_s = float(ws)/hs # Aspect ratio
+    extent_s = float(area)/(ws*hs) # Extent
+    hull = cv2.convexHull(c) # Solidity
+    hull_area = cv2.contourArea(hull)
+    if (hull_area != 0):
+        solidity = float(area)/hull_area
+    else:
+        solidity = 0
+    aspect_ratio_wh = float(w)/h  # Aspect ratio
+    extent = float(area)/(w*h) # Extent
+    (xe,ye),(MA,ma),ae = cv2.fitEllipse(c)
+    ed = np.sqrt(4*area/np.pi) # Equivalent Diameter
+    ratio_ell = float(ma)/MA
+    perimeter = cv2.arcLength(c, True) # Arclength
+    p_centroid = np.array([float(cx), float(cy)])
+    p_masscenter = np.array([float(x), float(y)])
+    is_cen_inside = cv2.pointPolygonTest(c, p_centroid, False) # Checking if centroid is inside
+    is_mce_inside = cv2.pointPolygonTest(c, p_masscenter, False) # Checking if mass center is inside    
+    return {
+        'isconvex': isconvex,
+        'area': area,
+        'aspect_ratio_wh_s': aspect_ratio_wh_s,
+        'extent_s': extent_s,
+        'solidity': solidity,
+        'aspect_ratio_wh': aspect_ratio_wh,
+        'extent': extent,
+        'ed': ed,
+        'ratio_ell': ratio_ell,
+        'perimeter': perimeter,
+        'is_cen_inside': is_cen_inside,
+        'is_mce_inside': is_mce_inside
+    }
+
+
+def csga(contours):
+    assert contours is not None, "image file could not be read, check with os.path.exists()"
+    if len(contours) == 1:
+        ga = cgr(contours[0])
+    else:
+        gal = []
+        for i in range(0, (len(contours)-1)):
+            gal.append(cgr(contours[i]))
+        isc = []
+        al = []
+        asps = []
+        exts = []
+        sol = []
+        asp = []
+        ext = []
+        ed = []
+        rate = []
+        per = []
+        isc = []
+        ism = []
+        for i in range(0, len(gal)):
+            isc.append(gal[0]['isconvex'])
+            al.append(gal[0]['area'])
+            asps.append(gal[0]['aspect_ratio_wh_s'])
+            exts.append(gal[0]['extent_s'])
+            sol.append(gal[0]['solidity'])
+            asp.append(gal[0]['aspect_ratio_wh'])
+            ext.append(gal[0]['extent'])
+            ed.append(gal[0]['ed'])
+            rate.append(gal[0]['ratio_ell'])
+            per.append(gal[0]['perimeter'])
+            isc.append(gal[0]['is_cen_inside'])
+            ism.append(gal[0]['is_mce_inside'])
+        isconvex = np.all(isc)
+        area = np.mean(al, axis = 0)
+        aspect_ratio_wh_s = np.mean(asps, axis = 0)
+        extent_s = np.mean(exts, axis = 0)
+        solidity = np.mean(sol, axis = 0)
+        aspect_ratio_wh = np.mean(asp, axis = 0)
+        extent = np.mean(ext, axis = 0)
+        ed = np.mean(ed, axis = 0)
+        ratio_ell = np.mean(rate, axis = 0)
+        perimeter = np.mean(per, axis = 0)
+        is_cen_inside = np.mean(isc, axis = 0)
+        is_mce_inside = np.mean(ism, axis = 0)
+        ga = {
+            'isconvex': isconvex,
+            'area': area,
+            'aspect_ratio_wh_s': aspect_ratio_wh_s,
+            'extent_s': extent_s,
+            'solidity': solidity,
+            'aspect_ratio_wh': aspect_ratio_wh,
+            'extent': extent,
+            'ed': ed,
+            'ratio_ell': ratio_ell,
+            'perimeter': perimeter,
+            'is_cen_inside': is_cen_inside,
+            'is_mce_inside': is_mce_inside
+        }
+    return ga
+
+# mask file mf
+def feature_summary(image, mf):
+    # Generate a data frame for masks and attributes
+    df = pd.DataFrame()
+    df['mask'] = range(1, (len(mf)+1))
+    df = df.assign(gmedian = None, rmedian = None, bmedian = None,
+                   gmean = None, rmean = None, bmean = None,
+                   gstd = None, rstd = None, bstd = None,
+                   isconvex = None, area = None, aspect_ratio_wh_s = None,
+                   extent_s = None, solidity = None, aspect_ratio_wh = None,
+                   extent = None, ed = None, ratio_ell = None,
+                   perimeter = None, is_cen_inside = None, is_mce_inside = None)
+    mm = [] # masked image
+    for i in range(0, len(mf)):
+        mm.append(cv2.bitwise_and(image, image, mask = mf[i]))
+    mib = []
+    mig = []
+    mir = []
+    for i in range(0, len(mm)):
+        mib.append((mm[i][:,:,0])[np.where((mm[i][:,:,0]) != 0)])
+        mig.append((mm[i][:,:,1])[np.where((mm[i][:,:,1]) != 0)])
+        mir.append((mm[i][:,:,2])[np.where((mm[i][:,:,2]) != 0)])
+    # with warnings.catch_warnings():
+    #     warnings.simplefilter("ignore", category=RuntimeWarning)
+    #     foo = np.nanmean(x, axis=1)
+    for i in range(0, len(mm)):
+        df.at[i, 'bmean'] = np.mean(mib[i], axis = 0)
+        df.at[i,'gmean'] = np.mean(mig[i], axis = 0)
+        df.at[i,'rmean'] = np.mean(mir[i], axis = 0)
+        df.at[i,'bmedian'] = np.median(mib[i], axis = 0)
+        df.at[i,'gmedian'] = np.median(mig[i], axis = 0)
+        df.at[i,'rmedian'] = np.median(mir[i], axis = 0)
+        df.at[i,'bstd'] = np.std(mib[i], axis = 0)
+        df.at[i,'gstd'] = np.std(mig[i], axis = 0)
+        df.at[i,'rstd'] = np.std(mir[i], axis = 0)
+    for i in range(0, len(mf)):
+        df.at[i, 'isconvex'] = csga(ffpcontour_noplot(image, mf, i))['isconvex']
+        df.at[i, 'area'] = csga(ffpcontour_noplot(image, mf, i))['area']
+        df.at[i, 'aspect_ratio_wh_s'] = csga(ffpcontour_noplot(image, mf, i))['aspect_ratio_wh_s']
+        df.at[i, 'extent_s'] = csga(ffpcontour_noplot(image, mf, i))['extent_s']
+        df.at[i, 'solidity'] = csga(ffpcontour_noplot(image, mf, i))['solidity']
+        df.at[i, 'aspect_ratio_wh'] = csga(ffpcontour_noplot(image, mf, i))['aspect_ratio_wh']
+        df.at[i, 'extent'] = csga(ffpcontour_noplot(image, mf, i))['extent']
+        df.at[i, 'ed'] = csga(ffpcontour_noplot(image, mf, i))['ed']
+        df.at[i, 'ratio_ell'] = csga(ffpcontour_noplot(image, mf, i))['ratio_ell']
+        df.at[i, 'perimeter'] = csga(ffpcontour_noplot(image, mf, i))['perimeter']
+        df.at[i, 'is_cen_inside'] = csga(ffpcontour_noplot(image, mf, i))['is_cen_inside']
+        df.at[i, 'is_mce_inside'] = csga(ffpcontour_noplot(image, mf, i))['is_mce_inside']
+    return df
+
